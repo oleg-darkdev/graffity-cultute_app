f3c510d7e8accd5509cc3d9d0a7c8fd9291902cd
DarkDev <darkdev_brand@riseup.net>
2024-03-30 09:22:50 +0200

    alfa v.

---
+++ src/lib/widgets/ui/landing/Faq.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { faqData } from '$lib/shared';
+</script>
+
+<div class="p-8">
+	<div class="mt-12 rounded-lg bg-white p-4 py-8 shadow-xl">
+		<h4 class="text-center text-4xl font-bold uppercase tracking-widest text-gray-800">FAQ</h4>
+		<p class="mt-2 text-center text-sm text-gray-600">
+			Here are some of the frequently asked questions
+		</p>
+		<div class="mt-12 space-y-12 px-2 xl:px-16">
+			{#each faqData as faq}
+				<div class="mt-4 flex">
+					<div>
+						<div class="flex h-16 items-center border-l-4 border-blue-600">
+							<span class="px-4 text-4xl text-blue-600">Q.</span>
+						</div>
+						<div class="flex h-16 items-center border-l-4 border-gray-400">
+							<span class="px-4 text-4xl text-gray-400">A.</span>
+						</div>
+					</div>
+					<div>
+						<div class="flex h-16 items-center">
+							<span class="text-lg font-bold text-blue-600">{faq.question}?</span>
+						</div>
+						<div class="flex items-center py-2">
+							{#each faq.answer as answer}
+								<p class="mb-1 text-gray-500">
+									{answer}
+								</p>
+							{/each}
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
