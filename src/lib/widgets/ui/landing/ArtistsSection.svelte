f81fd68523268fac219951940bbc56b10d813a8b
DarkDev <darkdev_brand@riseup.net>
2024-03-30 09:22:38 +0200

    stable v.

---
+++ src/lib/widgets/ui/landing/ArtistsSection.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { ArtistCard } from '$lib/entities';
+	import { extensions } from '$lib/shared';
+</script>
+
+<section class="min-h-screen w-full flex justify-center">
+	<a name="artists" />
+
+	<div class="w-8/12  flex flex-wrap flex-row justify-between  ">
+    {#each extensions as extension}
+		<ArtistCard {extension} />
+	{/each}
+  </div>
+</section>
