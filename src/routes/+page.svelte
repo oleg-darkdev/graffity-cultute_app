49ae501167bd91ea98f60430e6eb5099cab55aa4
DarkDev <darkdev_brand@riseup.net>
2024-03-30 09:17:58 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { StylesList, Hero, Faq, ArtistsSection } from '$lib/widgets';
+</script>
+
+<svelte:head>
+	<title>Graffity wars boardgame</title>
+</svelte:head>
+
+<Hero />
+
+<StylesList />
+
+<ArtistsSection />
+
+<Faq />
---
+++ src/routes/+page.ts
@@ -0,0 +1,5 @@
+import '@fontsource/fira-mono/index.css';
+import '@fontsource/inter/index.css';
+import '@fontsource/poppins/index.css';
+
+export const prerender = true;
