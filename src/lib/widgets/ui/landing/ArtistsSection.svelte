87a2290f6279b06bf0faded9896e417c7bd66935
DarkDev <darkdev_brand@riseup.net>
2024-03-30 16:17:56 +0200

    add heading

--- src/lib/widgets/ui/landing/ArtistsSection.svelte
+++ src/lib/widgets/ui/landing/ArtistsSection.svelte
@@ -3,12 +3,14 @@
 	import { extensions } from '$lib/shared';
 </script>
 
-<section class="min-h-screen w-full flex justify-center">
+<section class="flex min-h-screen w-full justify-center flex-col items-center pt-20">
 	<a name="artists" />
 
-	<div class="w-8/12  flex flex-wrap flex-row justify-between  ">
-    {#each extensions as extension}
-		<ArtistCard {extension} />
-	{/each}
-  </div>
+	<h2 class="font-graffity title max-w-xl lg:text-6xl text-center">Artists by countries</h2>
+
+	<div class="flex  w-8/12 flex-row flex-wrap justify-between  ">
+		{#each extensions as extension}
+			<ArtistCard {extension} />
+		{/each}
+	</div>
 </section>
