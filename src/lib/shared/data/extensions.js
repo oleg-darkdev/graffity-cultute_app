c496c8f8d41b7e364d40b265e6e100b2b59aef10
DarkDev <darkdev_brand@riseup.net>
2024-03-30 09:20:05 +0200

    upload data

---
+++ src/lib/shared/data/extensions.js
@@ -0,0 +1,186 @@
+const artistExtensionData = [
+	{
+		title: 'Albany artists',
+		country: 'Albany',
+		price: '30',
+		img: 'albany.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Austria artists',
+		country: 'Austria',
+		price: '30',
+		img: 'austria.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Ukraine artists',
+		country: 'Ukraine',
+		price: '30',
+		img: 'ukraine.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Russia artists',
+		country: 'Russia',
+		price: '30',
+		img: 'russia.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Turkey artists',
+		country: 'Turkey',
+		price: '30',
+		img: 'turkey.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Sweden artists',
+		country: 'Sweden',
+		price: '30',
+		img: 'sweden.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Spain artists',
+		country: 'Spain',
+		price: '30',
+		img: 'spain.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Italy artists',
+		country: 'Italy',
+		price: '30',
+		img: 'italy.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Germany artists',
+		country: 'Germany',
+		price: '30',
+		img: 'germany.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Poland artists',
+		country: 'Poland',
+		price: '30',
+		img: 'poland.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'France artists',
+		country: 'France',
+		price: '30',
+		img: 'france.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'England artists',
+		country: 'England',
+		price: '30',
+		img: 'england.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Croatia artists',
+		country: 'Croatia',
+		price: '30',
+		img: 'croatia.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	},
+	{
+		title: 'Czech artists',
+		country: 'Czech',
+		price: '30',
+		img: 'czech.png',
+		cardsInclude: 40,
+		desc: [
+			{
+				title: 'Artists',
+				count: ''
+			}
+		]
+	}
+];
+
+export default artistExtensionData;
---
+++ src/lib/shared/data/faqData.js
@@ -0,0 +1,14 @@
+const faqData = [
+	{
+		question: `Lorem ipsum dolor sit amet consectetur adipisicing elit`,
+		answer: [
+			`Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, dignissimos. Neque
+									eos, dignissimos provident reiciendis debitis repudiandae commodi perferendis et
+									itaque, similique fugiat cumque impedit iusto vitae dolorum. Nostrum, fugit!`,
+			``,
+			``
+		]
+	}
+];
+
+export default faqData;
