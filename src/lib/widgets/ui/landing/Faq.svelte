d0415c454b6c3a6ce8b483d75e88cf19a365366e
DarkDev <darkdev_brand@riseup.net>
2024-03-30 16:16:41 +0200

    upd font

--- src/lib/widgets/ui/landing/Faq.svelte
+++ src/lib/widgets/ui/landing/Faq.svelte
@@ -4,7 +4,8 @@
 
 <div class="p-8">
 	<div class="mt-12 rounded-lg bg-white p-4 py-8 shadow-xl">
-		<h4 class="text-center text-4xl font-bold uppercase tracking-widest text-gray-800">FAQ</h4>
+
+		<h4 class="text-center lg:text-6xl font-bold uppercase tracking-widest text-black">FAQ</h4>
 		<p class="mt-2 text-center text-sm text-gray-600">
 			Here are some of the frequently asked questions
 		</p>
@@ -12,16 +13,16 @@
 			{#each faqData as faq}
 				<div class="mt-4 flex">
 					<div>
-						<div class="flex h-16 items-center border-l-4 border-blue-600">
-							<span class="px-4 text-4xl text-blue-600">Q.</span>
+						<div class="flex h-16 items-center border-l-4 border-black">
+							<span class="px-4 text-4xl text-black font-graffity">Q.</span>
 						</div>
 						<div class="flex h-16 items-center border-l-4 border-gray-400">
-							<span class="px-4 text-4xl text-gray-400">A.</span>
+							<span class="px-4 text-4xl text-gray-400 font-graffity">A.</span>
 						</div>
 					</div>
 					<div>
 						<div class="flex h-16 items-center">
-							<span class="text-lg font-bold text-blue-600">{faq.question}?</span>
+							<span class="text-lg font-bold text-black ">{faq.question}?</span>
 						</div>
 						<div class="flex items-center py-2">
 							{#each faq.answer as answer}
