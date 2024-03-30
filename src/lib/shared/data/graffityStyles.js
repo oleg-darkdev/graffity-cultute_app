2a910fd7cc60edf9c9746d9f4ae4d7a6bb4758ae
DarkDev <darkdev_brand@riseup.net>
2024-03-30 09:22:22 +0200

    add styles list

---
+++ src/lib/shared/data/graffityStyles.js
@@ -0,0 +1,143 @@
+const styles = [
+	{
+		title: 'Tags',
+		example: 'tags.jpg',
+		articleLink: '/tags',
+		shortDesc: ['A tag can be defined as a stylised signature of the graffiti artist’s name.'],
+		desc: [
+			'The most common tools for graffiti tags include spray paint and marker pens.',
+			'Tags can be found in every graffiti culture worldwide because they’re the earliest method of modern-day graffiti – with graffiti writers like Cornbread from Philadelphia being the first to do a graffiti tag in 1965.'
+		]
+	},
+	{
+		title: 'Throw-ups',
+		example: '',
+		articleLink: '/',
+		shortDesc: [
+			'Throw-ups are a style of graffiti characterised by rounded bubble letters with minimal negative space painted quickly (or ‘thrown-up’) on a surface.'
+		],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	},
+
+	{
+		title: '',
+		example: '',
+		articleLink: '/',
+		shortDesc: [''],
+		desc: ['', '']
+	}
+];
+
+export default styles;
