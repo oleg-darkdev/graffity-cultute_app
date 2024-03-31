d268dd21167273e122421c3d9c4a8253eb21c219
DarkDev <darkdev_brand@riseup.net>
2024-03-31 19:00:52 +0300

    upd api

--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -1,6 +1,7 @@
 import graffityStyles from './data/graffityStyles';
 import faqData from './data/faqData';
 import extensions from './data/extensions';
+import ShowMoreDataBtn from './ui/ShowMoreDataBtn.svelte';
 
 
-export { graffityStyles, faqData, extensions };
+export { graffityStyles, faqData, extensions, ShowMoreDataBtn };
--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -2,8 +2,9 @@ import StylesList from './ui/landing/StylesList.svelte';
 import Hero from './ui/landing/Hero.svelte';
 import Faq from './ui/landing/Faq.svelte';
 import About from './ui/landing/About.svelte';
+import AntySystemsProject from './ui/landing/AntySystemsProject.svelte';
 import ArtistsSection from './ui/landing/ArtistsSection.svelte';
 import VerticalSlider from './ui/app/VerticalSlider.svelte';
 
 
-export { StylesList, Hero, Faq, ArtistsSection, VerticalSlider, About };
+export { StylesList, Hero, AntySystemsProject, Faq, ArtistsSection, VerticalSlider, About };
