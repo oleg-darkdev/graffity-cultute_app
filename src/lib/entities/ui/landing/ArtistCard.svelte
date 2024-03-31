2b9adc7ed800da40a43fa0299e2b58113c197c35
DarkDev <darkdev_brand@riseup.net>
2024-03-31 19:00:09 +0300

    refactoring

--- src/lib/entities/ui/landing/ArtistCard.svelte
+++ src/lib/entities/ui/landing/ArtistCard.svelte
@@ -1,156 +1,135 @@
 <script>
 	export let extension;
-
-	import { onMount } from 'svelte';
-
-	// onMount(() => {
-	// 	let graphElem = document.querySelector('.imgBx');
-
-	// 	graphElem.setAttribute('data-before', extension.country);
-	// });
 </script>
 
 <div
-	class=" group my-6 h-auto border-2  border-neutral-900 hover:delay-550 hover:-translate-y-1 hover:scale-105 hover:border-yellow-400 hover:transition hover:duration-1000  hover:ease-in-out"
+	class="hover:delay-550 group mb-10 border-2 border-neutral-900 pt-4 hover:-translate-y-1 hover:scale-105 hover:border-yellow-400  hover:transition hover:duration-1000  hover:ease-in-out"
 >
-	<div class="container  max-w-md ">
-		<div
-			class=" imgBx w-full bg-neutral-900 text-neutral-700 group-hover:bg-yellow-600 group-hover:text-black "
-		>
-			<img
-				class=""
-				style="max-width: 600px;"
-				src="/images/extensions/{extension.img}"
-				alt="{extension.title} extension"
-			/>
+	<div class="wrapper mx-auto h-full">
+		<div class="overviewInfo ">
+			<div class="relative mb-14 flex h-[250px] flex-col justify-between ">
+				<div class="mb-3 text-xl">
+					<span>Artists</span>
+
+					<h3 class="group-hover:text-yellow-400">{extension.artists.length}</h3>
+				</div>
+				<div class="mb-3 text-xl">
+					<span>Relise</span>
+
+					<h3 class="group-hover:text-yellow-400">{extension.relise}</h3>
+				</div>
+				<div class="mb-3 text-xl">
+					<span>Cards</span>
+
+					<h3 class="group-hover:text-yellow-400">{extension.cards}</h3>
+				</div>
+
+				{#each extension.features as data}
+					<div class="mb-3 text-xl">
+						<span>{data.title}</span>
+
+						<h3 class="group-hover:text-yellow-400">{extension.subTitle}</h3>
+					</div>
+				{/each}
+				<div class="text-xl ">
+					<span>Country</span>
+
+					<h3 class="group-hover:text-yellow-400">{extension.country}</h3>
+				</div>
+				<!-- <div class="-mt-2 mb-4 text-xl">
+					<span>PRICE</span>
+
+					<h3 class="font-dollar -mt-2 text-3xl group-hover:text-yellow-400 ">
+						$ <span class="font-graffity">50</span>
+					</h3>
+				</div> -->
+
+				<div class="productImage">
+					<img
+						src="/images/extensions/{extension.img}"
+						alt="product: boardgame graffiti chronicles image"
+					/>
+				</div>
+			</div>
 		</div>
-		<div class="details">
-			<div class="content prose   pt-4">
-				<!-- <p>Quantity of cards: <span>{extension.cardsInclude}</span></p> -->
+		<!-- overview info -->
 
-				<p class="mb-2 text-zinc-700 group-hover:text-neutral-400">
-					This extension includes cards with the art of graffiti artists from the country of <span
-						class="text-neutral-300 group-hover:text-yellow-600">{extension.country}</span
-					>.
-				</p>
+		<div
+			class="productSpecifications h-full group-hover:bg-yellow-400  group-hover:transition  group-hover:duration-1000   group-hover:ease-in-out"
+		>
+			<h1 class="mt-6 mb-2 text-2xl group-hover:text-black">{extension.title}</h1>
+			<p class="mb-2 text-zinc-700 group-hover:text-black">
+				This extension includes cards with the art of graffiti artists from the country of <a
+					href="/"
+					target="_blank"
+					class="font-bold text-yellow-600 underline group-hover:text-black">{extension.country}</a
+				>.
+			</p>
 
-				<p class="mb-10 text-zinc-700 group-hover:text-neutral-400">
-					List of graffiti artists in this extension: <br />
-					{#each extension.artists as artist}
-						<span class="mr-1"
-							><a class="group-hover:text-yellow-600 " href={artist.link}
-								>{artist.nickName},
-							</a></span
-						>
-					{/each}
-				</p>
+			<p class="mb-10 text-zinc-700 group-hover:text-black">
+				List of graffiti artists in this extension: <br />
+				{#each extension.artists as artist}
+					<span class="mr-1"
+						><a
+							class="font-bold text-yellow-600 underline group-hover:text-black"
+							target="_blank"
+							href={artist.link}
+							>{artist.nickName},
+						</a></span
+					>
+				{/each}
+			</p>
 
-				<h3 class="font-dollar group-hover:text-yellow-400">
-					$ <span class="group-hover:text-yellow-400">{extension.price}</span>
+			<div class="flex flex-row justify-between ">
+				<h3 class="font-dollar text-6xl text-yellow-400 group-hover:text-black">
+					$ <span class="font-graffity text-yellow-400 group-hover:text-black"
+						>{extension.price}</span
+					>
 				</h3>
+
 				<button
-					class="mb-6 bg-black  text-white hover:bg-yellow-200 group-hover:bg-yellow-400 group-hover:text-black"
+					class="font-graffity mb-6 bg-yellow-400   px-3 py-5 text-2xl text-black group-hover:bg-black  group-hover:text-yellow-400"
 					>Buy Now</button
 				>
 			</div>
 		</div>
+		<!-- product specificaiton -->
 	</div>
 </div>
 
 <style>
-	.container {
-		position: relative;
-		height: 400px;
-		margin: 20px;
+	.wrapper {
+		width: 450px;
+		overflow: hidden;
 	}
 
-	.container .imgBx {
-		position: relative;
-		height: 100%;
-		transition: 0.3s linear;
+	.overviewInfo,
+	.productSpecifications {
+		padding: 24px;
 	}
 
-	.container .imgBx:before {
-		content: 'Extension';
-		/* content: attr(data-before); */
-		font-family: 'graffity';
+	.productImage {
 		position: absolute;
-		top: 0px;
-		left: 40px;
-		/* opacity: 0.2; */
-		font-size: 5em;
+		max-width: 400px;
+		width: auto;
+		height: 280px;
+		transform: rotate(-90deg) translate(-56px, 66px);
+		transition: ease 2s all;
+	}
+	.productImage img {
+		width: 100%;
+		height: 400px;
 	}
 
-	.container .imgBx img {
-		position: relative;
-		transform: rotate(-5deg);
-		left: -70px;
-		bottom: -20px;
-		transition: 0.9s linear;
-	}
+	.checkoutButton {
+		display: flex;
+		width: 100%;
 
-	/* .container .details h2 {
-		padding: 0;
-		font-size: 2.4em;
-		line-height: 1em;
-	} */
-
-	.container .details h2 span {
-		font-size: 0.4em;
-		text-transform: uppercase;
-		letter-spacing: 2px;
-	}
-
-	.container .details p {
-		max-width: 85%;
-		/* font-size: 15px; */
-	}
-
-	.container .details h3 {
-		margin: 0;
-		padding: 0;
-		font-size: 2.5em;
-		float: left;
-	}
-	.container .details button {
-		border: none;
-		outline: none;
-		padding: 15px 20px;
-		margin-top: 5px;
-		font-size: 16px;
-		letter-spacing: 1px;
-		text-transform: uppercase;
-		font-weight: 600;
-		border-radius: 40px;
-		float: right;
-	}
-
-	/* responsive */
-	@media (max-width: 900px) {
-		.container {
-			height: auto;
-		}
-		.container .imgBx {
-			padding: 40px;
-			box-sizing: border-box;
-			width: 100% !important;
-			height: auto;
-			text-align: center;
-			overflow: hidden;
-		}
-		.container .imgBx img {
-			left: initial;
-			/* max-width: 100%; */
-			transform: rotate(0deg);
-		}
-		.details {
-			width: 100% !important;
-			height: auto;
-			padding: 20px;
-		}
-		.container .details p {
-			margin-left: 0;
-			max-width: 100%;
-		}
+		overflow: hidden;
+		box-shadow: -2px -2px 2px 0px rgba(80, 80, 80, 0.1), 2px 2px 3px 0px rgba(12, 12, 12, 0.3),
+			inset 0px 0px 0px 2px rgba(80, 80, 80, 0.2);
+		margin-top: 30px;
+		justify-content: space-between;
+		align-items: center;
 	}
 </style>
