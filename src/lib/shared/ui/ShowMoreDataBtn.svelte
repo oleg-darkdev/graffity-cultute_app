8058d1e0af5357dce1c7619731bc6717289891d3
DarkDev <darkdev_brand@riseup.net>
2024-03-31 19:01:17 +0300

    beta  v.

---
+++ src/lib/shared/ui/ShowMoreDataBtn.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let showFull, link, title;
+</script>
+
+<!-- <button class="px-5 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 lg:px-10 rounded-xl hover:bg-blue-700  focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Get bundle</button> -->
+
+<a href={link} on:click={() => (showFull = !showFull)} role="button" class="w-[300px] text-center mb-10 mt-4 font-graffity text-2xl transition duration-500 ease-in-out transform focus:outline-none hover:bg-yellow-400 px-4 py-6 text-2xl uppercase border-2 border-yellow-400 text-yellow-400 hover:text-black">{showFull ? 'Hide' : 'SHOW'} {title}</a>
+
+<style>
+</style>
