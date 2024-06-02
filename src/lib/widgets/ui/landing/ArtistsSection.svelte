8aa1cec653e47320784cf76e73ca532e69f4bfee
DarkDev <darkdev_brand@riseup.net>
2024-06-02 16:12:31 +0300

    upd width

--- src/lib/widgets/ui/landing/ArtistsSection.svelte
+++ src/lib/widgets/ui/landing/ArtistsSection.svelte
@@ -6,14 +6,14 @@
 </script>
 
 <section class="my-20 flex min-h-screen w-full flex-col items-center justify-center pb-20 ">
-	<h2 class="font-graffity title max-w-xl text-center text-3xl lg:text-6xl">
+	<h2 class="font-graffity mb-10 title max-w-xl text-center text-3xl lg:text-6xl">
 		Artists by countries
 	</h2>
 
 	<a name="artists" />
 
-	<div class="flex  w-8/12 w-full  flex-row flex-wrap lg:justify-between xl:justify-between justify-center   ">
-		{#each showFull ? extensions : extensions.slice(0, 4) as extension}
+	<div class="flex  lg:w-10/12 xl:w-10/12 w-full  flex-row flex-wrap lg:justify-between xl:justify-between justify-center   ">
+		{#each showFull ? extensions : extensions.slice(0, 6) as extension}
 			<ArtistCard {extension} />
 		{/each}
 	</div>
