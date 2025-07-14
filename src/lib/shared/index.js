edd06da9c3bec13a2af5999fc40ff1a379b9704c
Bear-software <linner_952@mail.ru>
2025-07-14 03:38:38 +0200

    fix filename

--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -2,7 +2,7 @@ import graffityStyles from './data/graffityStyles';
 import faqData from './data/faqData';
 import extensions from './data/extensions';
 import ShowMoreDataBtn from './ui/ShowMoreDataBtn.svelte';
-import links from './ui/navigation.svelte';
+import links from './data/navigation';
 
 
 export { graffityStyles, links, faqData, extensions, ShowMoreDataBtn };
